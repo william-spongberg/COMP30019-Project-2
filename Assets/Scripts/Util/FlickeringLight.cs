7dfa7fd79e27b115afb0701ae0e1caa7f9de3809
William Spongberg <william.spongberg@outlook.com>
2024-10-01 11:01:02 +1000

    Revert "Merge pull request #5 from feit-comp30019/will-dev"
    This reverts commit 0928224a1d914eee96806b04fbd351e64c963887, reversing
    changes made to f83a7a1562d9c30bc34e007dbb4000327376053e.

--- Assets/Scripts/Util/FlickeringLight.cs
+++ Assets/Scripts/Util/FlickeringLight.cs
@@ -2,30 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlickerControl : MonoBehaviour
+public class FlickerControl : MonoBehaviour {
+    public bool isFlickering = false;
+    public float timeDelay;
+
+    public GameObject lightObject;
+
+    void Update()
 {
-    [SerializeField]
-    private float timeDelay;
-
-    void Start()
+    if (isFlickering == false)
     {
         StartCoroutine(FlickeringLight());
-    }
-
-    // if disabled by cache, restart flickering again
-    void OnEnable() {
-        StartCoroutine(FlickeringLight());
-    }
-
-    IEnumerator FlickeringLight()
-    {
-        while(true) {
-            GetComponent<Light>().enabled = false;
-            timeDelay = Random.Range(0.01f, 0.5f);
-            yield return new WaitForSeconds(timeDelay);
-            GetComponent<Light>().enabled = true;
-            timeDelay = Random.Range(0.01f, 0.5f);
-            yield return new WaitForSeconds(timeDelay);
-        }
     }
 }
+            IEnumerator FlickeringLight()
+            {
+                isFlickering = true;
+                lightObject.GetComponent<Light>().enabled = false;
+                timeDelay = Random.Range(0.01f, 0.5f);
+                yield return new WaitForSeconds(timeDelay);
+                lightObject.GetComponent<Light>().enabled = true;
+                timeDelay = Random.Range(0.01f, 0.5f);
+                yield return new WaitForSeconds(timeDelay);
+                isFlickering = false;
+            }
+        }
+    
