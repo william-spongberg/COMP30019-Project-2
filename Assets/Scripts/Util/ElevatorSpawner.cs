8c3c1f226e302c49fc4580637d8254dd2cd5e1cc
KchinD <chindk@student.unimelb.edu.au>
2024-10-29 03:19:31 +1100

    Refined the waves on level 1 and also added additional dialogue

--- Assets/Scripts/Util/ElevatorSpawner.cs
+++ Assets/Scripts/Util/ElevatorSpawner.cs
@@ -15,7 +15,6 @@ public class ElevatorSpawner : MonoBehaviour
     private List<GameObject> objects = new();
     
     public int trigger;
-    public int[] waves;
     private GameObject player;
     public Counter Tracker;
     private bool spawned;
@@ -48,17 +47,5 @@ public class ElevatorSpawner : MonoBehaviour
         GameObject newObj = Instantiate(objects[randomIndex], pos, Quaternion.identity);
         entitySpawned += 1;
         Tracker.IncreaseSpawn(1);
-    }
-
-    public void SpawnWave(int amount){
-        for(int i = 0; i < amount; i++){
-            SpawnNPC();
-        }
-    }
-
-    public void LevelSpawn(int[] waves){
-        for(int j = 0; j < waves.Length; j++){
-            SpawnWave(waves[j]);
-        }
     }
 }
