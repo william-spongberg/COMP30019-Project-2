db69ace74265db6aec80d51edae2d43f93069b19
William Spongberg <william.spongberg@outlook.com>
2024-10-29 22:23:41 +1100

    Meta file updates

--- Assets/Scripts/Enemy/BossHealthSystem.cs
+++ Assets/Scripts/Enemy/BossHealthSystem.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossHealthSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider healthSlider; // Reference to the slider UI element
+    private int maxHealth;
+
+    public void SetMaxHealth(int health)
     {
+        
         maxHealth = health;
         healthSlider.maxValue = maxHealth;
         healthSlider.value = maxHealth; // Initialize the slider to full health
@@ -17,4 +21,5 @@ public class BossHealthSystem : MonoBehaviour
         // Update the slider value to reflect the current health
         healthSlider.value = health;
     }
+
 } 
